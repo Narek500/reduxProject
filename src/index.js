48ddf47145e0af53b1d19b5dbedfe5c7496f5d71
ProgerDav <davit.gyulnazaryan@tumo.org>
2019-08-15 21:11:38 +0400

    Search Function Created

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ let initialState = [
     { id: Date.now().toString() + 1, name: 'Item 1', status: false },
     { id: Date.now().toString() + 2, name: 'Item 2', status: false },
     { id: Date.now().toString() + 3, name: 'Item 3', status: false },
-    { id: Date.now().toString() + 4, name: 'search test', status: false },
+    { id: Date.now().toString() + 4, name: 'Item for search', status: false },
 ]
 
 function rootReducer(state = initialState, action) {
